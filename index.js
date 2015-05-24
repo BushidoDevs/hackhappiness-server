a953e2361dc942daf058334179586f465874aa91
Jorge del Casar <jorge.casar@gmail.com>
2015-05-24 18:45:31 +0200

    remove clustering

--- index.js
+++ index.js
@@ -1,30 +1,22 @@
-var throng = require('throng');
-throng(start, {
-  workers: process.env.WEB_CONCURRENCY || 1,
-  lifetime: Infinity
+var deployd = require('deployd');
+
+var server = deployd({
+  port: process.env.PORT || 3000,
+  env: process.env.NODE_ENV || 'development',
+  db: {
+    connectionString: process.env.MONGOLAB_URI || 'mongodb://localhost:27017/hackhappiness'
+  }
 });
 
-function start() {
-  var deployd = require('deployd');
-  
-  var server = deployd({
-    port: process.env.PORT || 3000,
-    env: process.env.NODE_ENV || 'development',
-    db: {
-      connectionString: process.env.MONGOLAB_URI || 'mongodb://localhost:27017/hackhappiness'
-    }
+server.listen();
+
+server.on('listening', function() {
+  console.log("Server is listening");
+});
+
+server.on('error', function(err) {
+  console.error(err);
+  process.nextTick(function() { // Give the server a chance to return an error
+    process.exit();
   });
-  
-  server.listen();
-  
-  server.on('listening', function() {
-    console.log("Server is listening");
-  });
-  
-  server.on('error', function(err) {
-    console.error(err);
-    process.nextTick(function() { // Give the server a chance to return an error
-      process.exit();
-    });
-  });
-}
+});
